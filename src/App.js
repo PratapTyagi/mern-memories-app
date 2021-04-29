98e8fcc258f19a7cfb06b126c67566ddb1a973bc
Pratap Tyagi <tyagipratap111@gmail.com>
2021-04-29 17:37:38 +0530

    Starter code with some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import "./App.css";
+import LOGO from "./ascets/memories.png";
+
+import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core";
+import Form from "./Forms/Form";
+import Post from "./Posts/Posts";
+
+import useStyle from "./styles";
 
 function App() {
+  const classes = useStyle();
   return (
-    <div className="App">
-      <h1>MERN Memories App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} varient="h2" align="center">
+          MERN-Memories-App
+        </Typography>
+        <img className={classes.image} src={LOGO} alt="LOGO" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={8}>
+              <Post />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
