ef40ada76db15651006ab0052376e9dfb04aba54
Pratap Tyagi <tyagipratap111@gmail.com>
2021-04-29 23:20:20 +0530

    Created a POST request through form

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -5,5 +5,16 @@ export const getPosts = () => async (dispatch) => {
     const { data } = await api.fetchPosts();
     const action = await { type: "FETCH_ALL", payload: data };
     dispatch(action);
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPosts = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,7 +3,7 @@ export default (posts = [], action) => {
     case "FETCH-ALL":
       return action.payload;
     case "CREATE":
-      return action.payload;
+      return [...posts, action.payload];
 
     default:
       return posts;
